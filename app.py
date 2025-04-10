df35442537a9d03a3fa9acb6f2275722d4f55599
Og`abek Egamberdiyev <99131243+bek125@users.noreply.github.com>
2025-04-10 12:04:17 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
-bot.remove_webhook()
-bot.set_webhook(url="https://sening-railway-url.up.railway.app/")
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Bot ishlayapti!", 200
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.get_json()
+    print(data)
+    return "ok", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
